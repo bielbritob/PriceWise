57026b9108eeb38a0bd8d17b26b27e650738d77b
bielbritob <bielbritob123@gmail.com>
2025-01-19 17:18:59 -0400

    update - testing stCloud -

--- testing/streamlitapp.py
+++ testing/streamlitapp.py
@@ -5,3 +5,9 @@ if st.button('Pesquisar IG'):
     with st.spinner('pesquisando'):
          os = subprocess.run(["python", "testing/scrapFile.py"], capture_output=True)
          st.write(os)
+
+if st.button('instalar zendriver'):
+    with st.spinner('instalando zendriver'):
+        zen = subprocess.run(['pip', 'install', 'zendriver'], capture_output=True)
+        st.write(zen)
+
