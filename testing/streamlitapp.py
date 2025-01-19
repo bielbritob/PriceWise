0172603b29b30ad1fed1bff22125b73533c8a3b3
bielbritob <bielbritob123@gmail.com>
2025-01-19 18:16:11 -0400

    update - testing stCloud -

--- testing/streamlitapp.py
+++ testing/streamlitapp.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import subprocess
+import os
 
 seletor =  st.checkbox('Script seletor de cidade? (apenas na 1ª vez)')
 produto = st.text_input('text input', placeholder='leite integral, uva, ovo...')
 if st.button('Pesquisar IG'):
     with st.spinner('pesquisando'):
         if seletor:
-             os = subprocess.run(["python",
+             os1 = subprocess.run(["python",
                                   "testing/scrapFile.py", produto, "True",
                                   "True"],
                                  capture_output=True)
-             st.write(os)
+             st.write(os1)
         else:
             os = subprocess.run(["python",
                                  "testing/scrapFile.py", produto, "False"],
                                 capture_output=True)
             st.write(os)
+
         screenshot_path = "/tmp/screenshot.png"
         if os.path.exists(screenshot_path):
             st.image(screenshot_path, caption="Screenshot da página")
